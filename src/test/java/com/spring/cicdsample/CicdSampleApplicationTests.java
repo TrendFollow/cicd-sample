f48856b1b3a7a6142766a36a8151ed2cca2f0d3f
TrendFollow <160304124+TrendFollow@users.noreply.github.com>
2024-09-02 00:44:51 +0900

    Initial commit

---
+++ src/test/java/com/spring/cicdsample/CicdSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cicdsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
